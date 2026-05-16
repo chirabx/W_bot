616ba6d1c7fe046d00d7f4bc4f3e3be34a019a27
chirabx <1819496201@qq.com>
2026-05-16 17:17:43 +0800

    导航微调优化

--- src/carry/src/print_id.cpp
+++ src/carry/src/print_id.cpp
@@ -95,19 +95,21 @@ void callback(const apriltags2_ros::AprilTagDetectionArray::ConstPtr &msg)
             // 远距离时先粗调（扩大 roll 调节范围），近距离再精调
             double roll_adjust_min = 50.0;
             double roll_adjust_max = 130.0;
+            bool slow_flag = true;
             if (z > 110)
             {
                 roll_adjust_min = 70.0;
                 roll_adjust_max = 110.0;
+                slow_flag = false;
             }
 
             if (roll <= -roll_adjust_min && roll >= -roll_adjust_max)
             {
-                Turn_safe(pub, 0.07, 6); // zuo
+                if(!slow_flag) Turn_safe(pub, 0.07, 6); // zuo
             }
             else if (roll >= roll_adjust_min && roll <= roll_adjust_max)
             {
-                Turn_safe(pub, -0.07, 6); // you
+                if(!slow_flag) Turn_safe(pub, -0.07, 6); // you
             }
             else // 倾斜角正常
             {
